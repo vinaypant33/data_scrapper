4152e663b9bc82ef2b94f875a50037274a58ebbb
Vinay Pant <vinaypant24@gmail.com>
2024-04-29 11:51:12 +0530

    Skeleton Code

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+Class for selenium : 
+
+Main App using the Tkinter or Kivy then make the interface as the exe file and the andorid file : 
+
+Make the code to be divided into multiple parts and then take each part to check and scrape the data : for single computer multithreading and for multiple computers 
+socket and divide the task into different components. 
+
+
+
+
+
+'''
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
